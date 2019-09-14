0f6caa5a222794e603426ef41f59e66a82630b93
Oliver Parkinson <olliepar01@gmail.com>
2019-09-14 18:38:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ride){
+  if (ride = 400) {
+    return "This one is on me!"
+  } 
+  if (distance <= 2000) {
+    return "I will gladly take your thirty bucks"
+  }
+  if (ride < 2500) {
+    return "No can do."
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  if (city = "NYC") {
+    return "Ok, sounds good."
+  }
+  return "No go."
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  if (tip <= 30) {
+    return "Thank you so much."
+  } else if (tip > 30) {
+    return "Thank you"
+  } else {
+      return 'Bye.'
+  }
 }
